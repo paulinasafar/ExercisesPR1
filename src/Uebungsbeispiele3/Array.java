9976289e7be2898bd18bce7ab27aab5987f00273
Paulina Safar <paulina.safar@yahoo.com>
2021-04-10 14:34:50 +0200

    Ubungsbeispiele2

---
+++ src/Uebungsbeispiele3/Array.java
@@ -0,0 +1,14 @@
+package Uebungsbeispiele3;
+
+public class Array {
+
+    public static void main(String[] args) {
+        System.out.println();
+       // Array[] arr = {0,15,48,5,89,90,36,122,-95,85,47,-5,2};
+
+    }
+
+    public static void arrangeOrder(int[] numbers) {
+//TODO: your code here…
+    }
+}
