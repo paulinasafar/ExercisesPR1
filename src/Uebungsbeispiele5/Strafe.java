ba056f79c234dea765bfd25bf8c35bf3e8404850
Paulina Safar <paulina.safar@yahoo.com>
2021-04-12 15:11:04 +0200

    Uebungsbeispiele5

---
+++ src/Uebungsbeispiele5/Strafe.java
@@ -0,0 +1,61 @@
+package Uebungsbeispiele5;
+
+public class Strafe {
+
+    String vorname;
+    String nachname;
+    String kennzeichen;
+    int strafnummer;
+    double strafe;
+    int anzahl;
+
+    public void strafe(int geschwindigkeitsueberschreitung) {
+        if (geschwindigkeitsueberschreitung >= 20) {
+            strafe += 30;
+            anzahl += 1;
+        } else if (geschwindigkeitsueberschreitung <= 30) {
+            strafe += 50;
+            anzahl += 1;
+        } else if (geschwindigkeitsueberschreitung <= 50) {
+            strafe += 100;
+            anzahl += 1;
+        } else if (geschwindigkeitsueberschreitung <= 100) {
+            strafe += 500;
+            anzahl += 1;
+        } else if (geschwindigkeitsueberschreitung) {
+            strafe += 1500;
+            anzahl += 1;
+        }
+    }
+        public void verbandspaket () {
+            strafe += 25;
+            anzahl += 1;
+        }
+        public void alkohol ( double wert){
+            if (wert <= 1.0) {
+                strafe += 100;
+                anzahl += 2;
+            } else if (wert <= 2.0) {
+                strafe += 400;
+                anzahl += 2;
+            } else if (wert <= 3.0) {
+                strafe += 1000;
+                anzahl += 2;
+            }
+            if (wert > 3.0) {
+                strafe += 5000;
+                anzahl += 2;
+            }
+        }
+            public double getStrafe() {
+            if (anzahl == 1) {
+                strafe = strafe * 0,7;}
+            else if (anzahl == 2) {
+                strafe = strafe * 0,8;}
+            else if (anzahl == 3) {
+                strafe = strafe * 0,9;}
+            else {strafe = strafe * 1;
+            }
+            return strafe;}
+
+}
---
+++ src/Uebungsbeispiele5/StrafeApp.java
@@ -0,0 +1,18 @@
+package Uebungsbeispiele5;
+
+public class StrafeApp {
+
+    public static void main(String[] args) {
+
+        Strafe strafe1 = new Strafe();
+        strafe1.vorname = "John";
+        strafe1.nachname = "Black"";
+        strafe1.kennzeichen = "G987UE";
+        strafe1.strafnummer = 12345;
+
+        strafe1.strafe()
+
+
+    }
+
+}
