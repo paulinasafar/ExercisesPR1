1db59ff6c7b8a8bfdccaa47add7fee1fd75e158f
Paulina Safar <paulina.safar@yahoo.com>
2021-04-16 15:20:09 +0200

    uebungstrawbery

--- src/uebungstrawbery/Erdbeerland.java
+++ src/uebungstrawbery/Erdbeerland.java
@@ -106,6 +106,8 @@ public class Erdbeerland {
         for (int i = 0; i < becherFüllung.length ; i++) {
                 netWeight = netWeight + WEIGHTS[becherFüllung[i]];
             }
+        netWeight = Math.round(netWeight*100);
+        netWeight = netWeight / 100;
         return netWeight;}
     }
 
