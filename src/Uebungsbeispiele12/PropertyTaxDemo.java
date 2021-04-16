177eaff9bb451dc90c13c5f85f3f37cd288bd80a
Paulina Safar <paulina.safar@yahoo.com>
2021-04-16 20:39:12 +0200

    uebungsbeispiele12

---
+++ src/Uebungsbeispiele12/PropertyTaxDemo.java
@@ -0,0 +1,88 @@
+package Uebungsbeispiele12;
+
+import javax.swing.plaf.basic.BasicSplitPaneUI;
+import java.util.Arrays;
+
+public class PropertyTaxDemo {
+    public static void main(String[] args) {
+
+        int[][] array1 = {{1, 30, 40},
+                {2, 50, 20},
+                {1, 25, 35},
+                {3, 15, 45},
+                {3, 30, 40},
+                {2, 30, 10},
+                {1, 35, 35},
+                {1, 25, 65},
+                {3, 15, 45},
+                {1, 10, 70},
+                {2, 30, 10},
+                {3, 75, 15}};
+
+        int[] kunden1 = {0, 4, 6, 8, 11, 4, 0, 11, 15, 7, 4, 24};
+
+        System.out.println(array1.length);
+        System.out.println(kunden1.length);
+
+        System.out.println("Area Array: " + Arrays.deepToString(calcArea(array1)));
+        System.out.println("Array with Taxes: " + Arrays.deepToString(calcTax(array1)));
+        System.out.println("Counter of Clients: " + counterKunden(kunden1));
+    }
+
+    public static int[][] calcArea(int[][] array1) {
+        int[][] array2 = new int[array1.length][2];
+
+        for (int i = 0; i < array1.length; i++) {
+            array2[i][0] = array1[i][0];
+            array2[i][1] = array1[i][1] * array1[i][1];
+        }
+
+        return array2;
+    }
+
+    public static double[][] calcTax(int[][] array1) {
+        double[][] array3 = new double[array1.length][2];
+
+        for (int i = 0; i < array3.length; i++) {
+            if (calcArea(array1)[i][0] == 1) {
+                array3[i][0] = calcArea(array1)[i][0];
+                array3[i][1] = calcArea(array1)[i][1] * 3.20;
+            } else if (calcArea(array1)[i][0] == 2) {
+                array3[i][0] = calcArea(array1)[i][0];
+                array3[i][1] = calcArea(array1)[i][1] * 2.10;
+            } else if (calcArea(array1)[i][0] == 3) {
+                array3[i][0] = calcArea(array1)[i][0];
+                array3[i][1] = calcArea(array1)[i][1] * 0.90;
+            }
+        }
+        return array3;
+    }
+
+    public static int counterKunden(int[] kunden1) {
+        int counter = 0;
+        int[] kunden3 = new int[kunden1.length];
+
+        for (int i = 0; i < kunden1.length; i++) {
+            for (int j = i + 1; j < kunden1.length; j++) {
+                kunden3[i] = kunden1[i];
+                if (kunden3[i] == kunden3[j]) {
+                    kunden3[j] = -1;
+                }
+            }
+        }
+        for (int j = 0; j < kunden3.length; j++) {
+            if (kunden3[j] >= 0)
+                counter++;
+        }
+        return counter;
+    }
+
+//    public static void calcProperty(int[][] array1, int[] kunden1) {
+//        int[][] kunden2 = new int[counterKunden(kunden1)][2];
+//
+//        for (int i = 0; i <; i++) {
+//
+//        }
+//    }
+
+}
